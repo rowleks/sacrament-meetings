9c9b8851054f792c2118d095b8cebbbded135bd7
Rowland Momoh <momohrowland@gmail.com>
2026-07-12 19:26:16 +0100

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main>
+        <h1>Home</h1>
+      </main>
+    </div>
+  );
+}
